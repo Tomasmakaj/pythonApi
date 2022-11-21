fd7656b64159d280b260484e8b2b2a7d07f52fe0
Tomas Makaj <Tmakajcoding@gmail.com>
2022-11-21 10:46:12 -0500

    visual

--- python_repos.py
+++ python_repos.py
@@ -91,3 +91,51 @@ print(f"Repository: {repo_dict ['html_url']}")
 print(f"Created: {repo_dict['created_at']}")
 print(f"Updated: {repo_dict['updated_at']}")
 print(f"Description: {repo_dict['description']}")
+
+# Here we print the values for a number of keys from the first repository dictionary.
+
+
+
+# Summarizing the Top Repositories
+# When we make a visualization for this data, we'll want to include more than one repository. 
+# Lets write a loop to print selected information about each repository the API call returns so we can include them in a visualization:
+
+import requests
+
+# Make an API call and store the response
+url = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Store API response in a variable
+response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+
+
+# Explore information about the repositories
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+
+print("\nSelected information about first repository:")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Description: {repo_dict['description']}")
+
+# We print an introfuctory message at & we loop through all the dictionaries in repo_dicts.
+# Here we loop through Name of Project, Owner, Stars & URL
+
+
+
+# Monitoring API Rate Limits
+# Most API's are rate limited, which means there's a limit to how many requests you can make in a certain amount of time.
+# To see if you're approaching GitHub's limits.
+
+
+
+
+
+# Visualizing Repositories Using Plotly
