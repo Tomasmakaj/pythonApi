1ed1bb49b77ccdf30fb22b28522b3359bac0c8da
Tomas Makaj <Tmakajcoding@gmail.com>
2022-11-21 11:52:25 -0500

    graph

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -33,3 +33,127 @@ my_layout = {
 
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='python_repos.html')
+
+# Import the bar class and offline modile from plotly.
+# Dont need to import the layout class because we'll use the dictionary approach to define the layout.
+# Dont need the code that process the API response because we're no longer in the exploratory phase
+# We create 2 empty lists to store the data we'll include in the initial chart
+
+
+
+# Refining Plotly Charts
+# Lets refine the chart's styling. You can include all the styling directives as key-value pairs in the data and my_layout dictionaries
+
+
+import requests
+
+from plotly.graph_objs import Bar
+from plotly import offline
+
+# Make an API call and store the response
+url = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+
+# Process the results
+response_dict = r.json()
+repo_dicts = response_dict['items']
+repo_names, stars = [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+    
+# Make a visualization.
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25'}
+    },
+    'opacity': 0.6,
+}]
+
+my_layout = {
+    'title': 'Most-Starred Python Projects on Github',
+    'titlefont': {'size': 28},
+    'xaxis': {
+        'title': 'Repository',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+        },
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {'size': 24},
+        'tickfont': {'size':14},
+        },
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
+
+
+
+# Adding Custom Tooltips
+# In plotly, you can hover the cursor over an individual bar to show the information that the bar represents.
+# This is commonly called a tooltip, and in this case, it currently shows the number of stars a project has. 
+# Lets create a custom tooltip to show each project's description as well as the projects owner.
+
+import requests
+
+from plotly.graph_objs import Bar
+from plotly import offline
+
+# Make an API call and store the response
+url = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+
+# Process the results
+response_dict = r.json()
+repo_dicts = response_dict['items']
+repo_names, stars, labels = [], [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+    
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
+    
+# Make a visualization.
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars,
+    'hovertext': labels,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    'opacity': 0.6,
+}]
+
+my_layout = {
+    'title': 'Most-Starred Python Projects on Github',
+    'titlefont': {'size': 28},
+    'xaxis': {
+        'title': 'Repository',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+        },
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {'size': 24},
+        'tickfont': {'size':14},
+        },
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
